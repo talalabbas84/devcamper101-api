700e73b8a0de961f7a1d0b30c16d355c57eff014
Talal Abbas <talalabbas84@gmail.com>
2020-04-26 06:04:19 +0500

    Logout token clear Prevent NoSqlInjection and sanitize data, XSS Protection & Security Headers, Rate Limiting, HPP & CORS setup

--- middleware/error.js
+++ middleware/error.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
     console.log(err.stack.red);
     // Mongoose bad ObjectId
     if (err.name === 'CastError') {
-        const message = `Resource not found with id of ${err.value}`;
+        const message = `Resource not found `;
         error = new ErrorResponse(message, 404);
     }
 
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const colors = require(`colors`);
 const fileupload = require('express-fileupload');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 const bootcamps = require(`./routes/bootcamps`);
 const courses = require(`./routes/courses`);
@@ -33,6 +39,28 @@ if (process.env.NODE_ENV === 'development') {
 app.use(fileupload());
 
 app.use(cookieParser());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+// Prevent XSS attacks
+app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100,
+});
+
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
